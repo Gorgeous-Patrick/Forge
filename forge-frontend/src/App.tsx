1d89e4e4da4a725d73ecad05afe86e52cd91f9b4
Gorgeous-Patrick <33488623+Gorgeous-Patrick@users.noreply.github.com>
2026-01-08 10:15:19 -0500

    feat: top bar added

--- forge-frontend/src/App.tsx
+++ forge-frontend/src/App.tsx
@@ -22,59 +22,69 @@ export default function App() {
       title: 'Break (10m)',
       start: todayAt(14, 50),
       end: todayAt(15, 0),
-      // 你后面可以用 extendedProps 标记类型
       extendedProps: { kind: 'break' },
     },
   ]
 
   return (
-    <Box p={4} maxW="980px" mx="auto">
-      <Flex align="baseline" gap={3}>
-        <Heading as="h1" size="xl" m={0}>
-          Forge
-        </Heading>
-        <Text opacity={0.7}>Calendar</Text>
-      </Flex>
+    <Box minH="100vh" bg="gray.50">
+      <Box
+        as="header"
+        bg="white"
+        borderBottomWidth="1px"
+        borderBottomColor="gray.200"
+        px={4}
+        py={3}
+      >
+        <Flex align="baseline" gap={3} maxW="980px" mx="auto">
+          <Heading as="h1" size="xl" m={0}>
+            Forge
+          </Heading>
+          <Text opacity={0.7}>Calendar</Text>
+        </Flex>
+      </Box>
 
-      <Box mt={3}>
-        <FullCalendar
-          plugins={[timeGridPlugin, interactionPlugin]}
-          initialView="timeGridDay"
-          headerToolbar={{
-            left: 'prev,next today',
-            center: 'title',
-            right: '',
-          }}
-          nowIndicator={true}
-          height="auto"
-          allDaySlot={false}
-          slotDuration="00:10:00"
-          slotLabelInterval="01:00"
-          expandRows={true}
-          weekends={true}
-          editable={true}
-          eventStartEditable={true}
-          eventDurationEditable={true}
-          events={events}
-          eventClick={(info) => {
-            const kind = info.event.extendedProps?.kind ?? 'task'
-            alert(`[${kind}] ${info.event.title}`)
-          }}
-          eventDrop={(info) => {
-            console.log('eventDrop:', {
-              id: info.event.id,
-              start: info.event.start,
-              end: info.event.end,
-            })
-          }}
-          eventResize={(info) => {
-            console.log('eventResize:', {
-              id: info.event.id,
-              start: info.event.start,
-              end: info.event.end,
-            })
-          }}
-        />
+      <Box p={4} maxW="980px" mx="auto">
+        <Box mt={3}>
+          <FullCalendar
+            plugins={[timeGridPlugin, interactionPlugin]}
+            initialView="timeGridDay"
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: '',
+            }}
+            nowIndicator={true}
+            height="auto"
+            allDaySlot={false}
+            slotDuration="00:10:00"
+            slotLabelInterval="01:00"
+            expandRows={true}
+            weekends={true}
+            editable={true}
+            eventStartEditable={true}
+            eventDurationEditable={true}
+            events={events}
+            eventClick={(info) => {
+              const kind = info.event.extendedProps?.kind ?? 'task'
+              alert(`[${kind}] ${info.event.title}`)
+            }}
+            eventDrop={(info) => {
+              console.log('eventDrop:', {
+                id: info.event.id,
+                start: info.event.start,
+                end: info.event.end,
+              })
+            }}
+            eventResize={(info) => {
+              console.log('eventResize:', {
+                id: info.event.id,
+                start: info.event.start,
+                end: info.event.end,
+              })
+            }}
+          />
+        </Box>
       </Box>
     </Box>
   )
