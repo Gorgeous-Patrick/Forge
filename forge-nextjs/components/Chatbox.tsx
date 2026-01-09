adc0e3389bbbe51ca2809bbd9e40459f07c717da
Patrick Li <baichuanli@yahoo.com>
2026-01-08 20:48:08 -0500

    feat: Thread partially working (not styled??)

--- forge-nextjs/components/Chatbox.tsx
+++ forge-nextjs/components/Chatbox.tsx
@@ -1,5 +1,8 @@
+import { AssistantIf } from '@assistant-ui/react'
 import { Thread } from './assistant-ui/thread'
 
+
 export function ChatboxComponent() {
-  return <Thread />
+  
+  return (<Thread />)
 }
