c9f92a97c0775d534d5eaba71ecc8aac0559354c
Patrick Li <baichuanli@yahoo.com>
2026-01-15 14:18:57 -0500

    feat: no vibe companion!

--- forge-nextjs/app/page.tsx
+++ forge-nextjs/app/page.tsx
@@ -1,12 +1,8 @@
 import App from "@/components/App";
-import {VibeKanbanWebCompanion} from "vibe-kanban-web-companion";
 
 
 export default function Home() {
   return (
-    <>
-    <VibeKanbanWebCompanion />
   <App />
-  </>
   );
 }
